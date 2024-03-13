b99c1ac207bcd84cfe6f9b8f61e18782a23dfaa2
HudsonJR777 <hudsonjunior@pkkgmail.com>
2024-03-12 21:50:56 -0300

    Subindo projeto finalizado

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.8,
+    spaceBetween: 19,
+    freeMode: true,
+ /*   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }, */
+});
+AOS.init({
+  duration:1000,
+  once: true,
+});
+
